0abd3df13643d5cec6975b76c4f3a697b661277f
Piotr Klibert <piotr.klibert@10clouds.com>
2017-03-31 01:56:53 +0200

    Some refactoring, made center of a curve draggable

--- demo/canvas.rkt
+++ demo/canvas.rkt
@@ -16,40 +16,58 @@
     (list->point coord)))
 
 (define default-font
-  (make-font #:size 28
-             #:weight 'bold
-             #:family 'roman))
+  (make-font #:size 28 #:weight 'bold #:family 'roman))
 
 (define curved-text-editor%
   (class canvas%
     ;; automatically initialized public fields
     (init-field [text           "Some curved text"]
                 [font           default-font]
-                [control-radius 15])
+                [control-radius 15]
+                [center-radius  20])
 
     ;; private fields
     (define controls       default-controls)
     (define active-control #false) ;; an index in controls vector or #false
+    (define active-center  #false)
     (define control-margin 5)
 
-
-    ;; this is an attempt to reduce the number of redraws, but
-    ;; I don't think it helped much
-    ;; TODO: profile and probably throw this away or maybe refactor
-    (define prev-mouse-pos     (make-point 0 0))
     (define min-animation-dist 3)
+    (define prev-mouse-pos (make-point 0 0))
     (define (worth-animating? cur-pos)
       (> (points-distance prev-mouse-pos cur-pos)
          min-animation-dist))
 
+
+    (define/private (get-control-radius)
+      (+ control-radius control-margin))
+
     (define/private (get-control n)
       (vector-ref controls n))
-    ;; move-active-control!: point? -> void?
-    (define/private (move-active-control! where)
-      (vector-set! controls active-control where))
 
+    (define/public (get-endpoints)
+      (vector->list controls))
 
-    ;; Controls (control points) helpers.
+    ;; compute the "center of mass" for the four controls
+    (define/public (get-center)
+      (let ([sum (apply + (map point->complex (get-endpoints)))])
+        (complex->point (/ sum 4))))
+
+    (define/private (get-own-dc)
+      (let ([dc (send this get-dc)])
+        (send dc set-smoothing 'smoothed)
+        dc))
+
+    ;; Helpers for handling mouse interaction with control points.
+    (define/private (nth-clicked? n pos)
+      (if (control-clicked? (get-control n) pos) n #f))
+
+    (define/private (control-clicked? ctrl pos)
+      (< (points-distance ctrl pos) (get-control-radius)))
+
+    (define/private (center-clicked? pos)
+      (< (points-distance (get-center) pos)
+         (+ center-radius control-margin)))
 
     ;; get-clicked-control: point -> (or/c number? #f)
     (define/private (get-clicked-control click-pos)
@@ -62,43 +80,23 @@
         [(nth-clicked? 3 click-pos)]
         [else #false]))
 
-    (define/private (nth-clicked? n pos)
-      (if (control-clicked? (get-control n) pos) n #f))
-    (define/private (control-clicked? ctrl pos)
-      (< (points-distance ctrl pos) (real-radius)))
-    (define/private (real-radius)
-      (+ control-radius control-margin))
-
-    (define/public (get-endpoints)
-      (list  (get-control 0)
-             (get-control 1)
-             (get-control 2)
-             (get-control 3)))
-    (define/public (get-center)
-      (match-let
-          ([(list a b c d) (map point->complex (get-endpoints))])
-        (complex->point (/ (+ a b c d) 4))))
+    ;; move-active-control!: point? -> void?
+    (define/private (move-active-control! where)
+      (move-control! active-control where))
 
 
     ;; Drawing helpers.
 
-    (define/private (get-own-dc)
-      ;; and set smoothing by the way
-      (let ([dc (send this get-dc)])
-        (send dc set-smoothing 'smoothed)
-        dc))
-
     (define/private (draw-supporting-curve dc)
-      (draw-path dc (for/list ([c controls]) (point->list c))))
+      (draw-path dc (map point->list (get-endpoints))))
+
     (define/private (draw-center dc)
-      (let* ([center (get-center)]
-             [x (point-x center)]
-             [y (point-y center)])
-        (draw-circle dc x y 20)))
+      (let* ([center (get-center)])
+        (draw-circle dc (point-x center) (point-y center) center-radius)))
+
     (define/private (draw-controls dc)
-      (for ([control-point controls])
-        (match (point->list control-point)
-          [(list x y) (draw-circle dc x y control-radius)])))
+      (for ([ctrl controls])
+        (draw-circle dc (point-x ctrl) (point-y ctrl) control-radius)))
 
     (define/private (draw-text dc)
       (send* dc
@@ -111,26 +109,34 @@
     ;; Public methods
     ;;
 
+    (define/public (move-control! which new-pos)
+      (vector-set! controls which new-pos))
+
+    ;; move-controls!: complex? -> void?
+    (define/public (move-curve! direction)
+      (set! controls
+        (vector-map (λ (x) (complex->point
+                            (+ direction (point->complex x))))
+                    controls)))
+
     ;; Rotate controls by an angle given as a number of degrees.
     (define/public (rotate-controls! direction [angle 5])
       (define center (get-center))
       (define degrees (if (equal? direction 'left) (- angle) angle))
       (define transform (cut rotate-point <> degrees center #:type 'degrees))
-
       (set! controls (vector-map transform controls))
       (send this refresh))
 
+    ;; Move all controls away or towards the center.
     (define/public (scale-controls! direction)
-      (define scale (if (or (equal? direction 'add)
-                            (equal? direction #\+))
-                        1.2
-                        0.8))
+      (define scale (if (member direction '(add #\+)) 1.2 0.8))
       (define transform (cut scale-point <> scale (get-center)))
-
       (set! controls (vector-map transform controls))
       (send this refresh))
+
 
     ;; GUI callbacks.
+    ;; =========================================================================
 
     ;; KEYBOARD events
     (define/override (on-char event)
@@ -146,30 +152,42 @@
     (define/override (on-event event)
       (let*
           ( ;; click position
-           [mouse-pos (make-point (send event get-x)
-                                  (send event get-y))]
+           [mouse-pos (make-point (send event get-x) (send event get-y))]
            ;; click event properties
            [down      (send event button-down?)]
            [up        (send event button-up?)]
-           [dragging  (send event dragging?)])
+           [dragging  (send event dragging?)]
+           [should-move? (worth-animating? mouse-pos)])
         (cond
           ;; manually dispatch on event type
           [(and dragging active-control)
-           (when (worth-animating? mouse-pos)
+           (when should-move?
              (set! prev-mouse-pos mouse-pos)
              (move-active-control! mouse-pos)
              (send this refresh-now))]
 
-          [down (let ([control (get-clicked-control mouse-pos)])
-                  (when control
-                    (set! active-control control)
-                    (set! prev-mouse-pos mouse-pos)))]
+          [(and dragging active-center)
+           (when should-move?
+             (move-curve! (- (point->complex mouse-pos)
+                             (point->complex prev-mouse-pos)))
+             (set! prev-mouse-pos mouse-pos)
+             (send this refresh-now))]
 
-          [up (set! active-control #false)])))
+          [down (cond
+                  [(get-clicked-control mouse-pos) =>
+                   (λ (control)
+                     (set! active-control control)
+                     (set! prev-mouse-pos mouse-pos))]
+                  [(center-clicked? mouse-pos)
+                   (set! active-center #true)
+                   (set! prev-mouse-pos mouse-pos)])]
+
+          [up (set! active-control #false)
+              (set! active-center #false)])))
+
 
     (define/override (on-paint)
-      (let
-          ([dc (get-own-dc)])
+      (let ([dc (get-own-dc)])
         (draw-supporting-curve dc)
         (draw-controls dc)
         (draw-center dc)
